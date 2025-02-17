86fdc209f17d847c0338709110bb7e0c57c19557
shanmukh0504 <shanmukh0504@gmail.com>
2025-02-17 12:16:52 +0530

    feat: new feat

--- packages/pack-b/src/index.js
+++ packages/pack-b/src/index.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var common_1 = require("common");
-var user = (0, common_1.createUser)('Shanmukh', 20);
-(0, common_1.showUser)(user);
+var pack_a_1 = require("pack-a");
+var user = (0, pack_a_1.createUser)('Shanmukh', 21);
+(0, pack_a_1.showUser)(user);
