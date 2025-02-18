0cfd82d71f721eb9a3aa4812896657941652e8de
shanmukh0504 <shanmukh0504@gmail.com>
2025-02-18 16:33:57 +0530

    fix: new fix

--- scripts/handle-release.sh
+++ scripts/handle-release.sh
@@ -48,7 +48,7 @@ increment_version() {
 }
 export -f increment_version
 
-yarn workspaces foreach --all --topological --no-private exec bash -c '
+yarn workspaces foreach --all --topological --no-private --exclude @shanmukh0504/monorepo exec bash -c '
   PACKAGE_NAME=$(jq -r .name package.json)
   CURRENT_VERSION=$(jq -r .version package.json)
   NEW_VERSION=$(increment_version $CURRENT_VERSION $VERSION_BUMP)
