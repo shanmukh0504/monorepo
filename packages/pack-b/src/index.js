874a5a22573d971a0d9bc91744833ef13c2d81ab
shanmukh0504 <shanmukh0504@gmail.com>
2025-02-18 09:44:04 +0000

    chore: commit release script and config changes

---
+++ packages/pack-b/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var pack_a_1 = require("pack-a");
+var user = (0, pack_a_1.createUser)('Shanmukh', 21);
+(0, pack_a_1.showUser)(user);
