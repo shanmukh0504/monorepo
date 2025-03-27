e5747d249233a19b6314473a3fbd4711d2b16936
shanmukh0504 <shanmukh0504@gmail.com>
2025-03-27 15:06:34 +0530

    fix: new fix

--- packages/pack-b/src/index.ts
+++ packages/pack-b/src/index.ts
@@ -2,7 +2,7 @@ import { createUser, showUser, User } from 'pack-a';
 import { createVehicle, showVehicle, Vehicle } from 'pack-c';
 
 const user: User = createUser('Shanmukh', 21);
-const vehicle: Vehicle = createVehicle('Car', 2021);
+const vehicle: Vehicle = createVehicle('Car', 2022);
 
 showUser(user);
 showVehicle(vehicle);
