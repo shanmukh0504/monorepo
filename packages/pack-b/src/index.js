380854f4c99670f221bfeacaff44ff2b03ccf6cd
shanmukh0504 <shanmukh0504@gmail.com>
2025-03-27 18:03:39 +0530

    Update packages

--- packages/pack-b/src/index.js
+++ packages/pack-b/src/index.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var pack_a_1 = require("pack-a");
 var pack_c_1 = require("pack-c");
 var user = (0, pack_a_1.createUser)('Shanmukh', 21);
-var vehicle = (0, pack_c_1.createVehicle)('Car', 2025);
+var vehicle = (0, pack_c_1.createVehicle)('Car', 2026);
 (0, pack_a_1.showUser)(user);
 (0, pack_c_1.showVehicle)(vehicle);
